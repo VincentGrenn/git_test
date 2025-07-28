7481a8e7d668261bec1fcac9f6749ee0d88f4a4c
VincentGrenn <s_vincent.grenn@ousd.org>
2025-07-28 07:46:27 +0000

    yes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import http from "http";
 import dotenv from "dotenv";
 import Middleware from "./middleware.js";
+import Data from "./data.js";
 
 dotenv.config();
 
@@ -13,6 +14,29 @@ const Server = http.createServer((req, res) => {
             res.end(JSON.stringify({
                 message: "This is the home page!",
             }));
+            return;
+        } else if (req.url === "/secret") {
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(JSON.stringify({
+                message: "How did you find this?",
+            }));
+            return;
+        } else if (req.url === "/data") {
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(Data);
+            return;
+        } else if (req.url.match(/\/data\/(\w+)/)) {
+            const Person = req.url.split("/")[2];
+            if (Data[Person]) {
+                res.writeHead(200, {"Content-Type": "application/json"});
+                res.end(JSON.stringify(Data[Person]));
+            } else {
+                res.writeHead(404, {"Content-Type": "application/json"});
+                res.end(JSON.stringify({
+                    message: "Person not found!",
+                }));
+            }
+            return;
         }
         res.writeHead(404, {"Content-Type": "application/json"});
         res.end(JSON.stringify({
